ee3fb6917b9e4c5d02ea1286ed89c91e5e45a767
ergcarvalho <ergcarvalho@rd.com.br>
2023-04-17 20:54:21 -0300

    iniciando estrutura do projeto

---
+++ src/test/java/br/com/egc/command/CommandApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.egc.command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
